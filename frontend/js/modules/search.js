d3d5bf233fde7fcea4f13cab663c845300ce81de
“Ksuyaresko” <Kiaresko@gmail.com>
2017-01-05 16:44:55 +0200

    search projects

--- frontend/js/modules/search.js
+++ frontend/js/modules/search.js
@@ -5,3 +5,9 @@ import template from 'content/search';
 
 var main = document.querySelector("main");
 main.innerHTML = template;
+
+
+import searchProjTemp from 'content/search-projects';
+
+var tabsMenu = document.querySelector(".search-results");
+tabsMenu.innerHTML = searchProjTemp;
