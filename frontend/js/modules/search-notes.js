81ca4e74d1438ea46899edbaf57947481e1dd678
“Ksuyaresko” <Kiaresko@gmail.com>
2017-01-06 15:10:23 +0200

    fixed user-list

--- frontend/js/modules/search-notes.js
+++ frontend/js/modules/search-notes.js
@@ -1,8 +1,11 @@
 import 'style.sass';
-
-
-import template from 'content/search-notes.haml';
+import template from 'content/search';
 
 var main = document.querySelector("main");
 main.innerHTML = template;
 
+var tabsMenu = document.querySelector(".search-results");
+
+import search from 'content/search-notes.haml';
+
+tabsMenu.innerHTML = search;
